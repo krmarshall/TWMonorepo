90859bd1e541e6868856043394c1d8360fc28d3d
krmarshall <59742056+krmarshall@users.noreply.github.com>
2024-06-05 20:42:26 -0600

    Bump: Vanilla 5.0.4 + Mods

--- TWInspector/src/pages/SkillTree.tsx
+++ TWInspector/src/pages/SkillTree.tsx
@@ -1,18 +1,13 @@
-import TopBar from '../../../TWPlanner/frontend/src/components/Planner/TopBar';
-import StatsDrawer from '../../../TWPlanner/frontend/src/components/Planner/StatsDrawer/StatsDrawer';
-import SkillTable from '../../../TWPlanner/frontend/src/components/Planner/SkillTable';
-import ExtrasDrawer from '../../../TWPlanner/frontend/src/components/Planner/ExtrasDrawer/ExtrasDrawer';
-
 const SkillTree = () => {
   return (
     <div className="grow mt-1 flex flex-col bg-gray-700 w-full border border-gray-500 rounded-md px-2 py-2 overflow-y-hidden overflow-x-hidden">
       {/* <TopBar isMobile={false} /> */}
       <div className="relative flex flex-row flex-nowrap grow max-h-[88vh] min-h-[50vh]">
-        <StatsDrawer />
+        {/* <StatsDrawer /> */}
 
-        <SkillTable faction={'other'} />
+        {/* <SkillTable faction={'other'} /> */}
       </div>
-      <ExtrasDrawer />
+      {/* <ExtrasDrawer /> */}
     </div>
   );
 };
