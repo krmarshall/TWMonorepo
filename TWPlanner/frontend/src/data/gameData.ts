cc6299e3f20e4a095b97cf41ba69032f763017ba
krmarshall <59742056+krmarshall@users.noreply.github.com>
2024-12-12 17:46:24 -0700

    Fix: Game Version

--- TWPlanner/frontend/src/data/gameData.ts
+++ TWPlanner/frontend/src/data/gameData.ts
@@ -37,7 +37,7 @@ const gameData: { [key: string]: GameDataInterface } = {
     image: gameImages['vanilla3'],
     factions: factions,
     characters: vanilla3CharactersMemes,
-    updated: 'Nov 7 2024 (5.3.1)',
+    updated: 'Dec 12 2024 (6.0.0)',
     category: 'Base',
   },
   sfo3: {
