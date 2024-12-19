dad81edb23abe3b453d3dec922a69398d5df43b2
krmarshall <59742056+krmarshall@users.noreply.github.com>
2024-12-19 11:41:52 -0700

    Bump: 6.0.1 + Mods

--- TWDataParser/src/lists/packInfo.ts
+++ TWDataParser/src/lists/packInfo.ts
@@ -36,19 +36,19 @@ export const modPackInfo: ModPackInfoInterface = {
     { pack: 'Radious_WH3_Mod_Part4', id: 2813892035 },
   ],
   sfo3: [{ pack: 'sfo_grimhammer_3_main', id: 2792731173 }],
-  lege3: [{ pack: '!scm_legendary', id: 3305404052 }], // { pack: '!str_legendary', id: 2826930183 } **change gameData.ts entry when you swap this back**
+  lege3: [{ pack: '!str_legendary', id: 2826930183 }], // { pack: '!scm_legendary', id: 3305404052 } **change gameData.ts entry when you swap this back**
   crys3: [{ pack: 'crys_leaders', id: 2880244265 }],
   scm3: [
     { pack: '!scm_lccp', id: 3236567583, name: 'LCCP' },
     { pack: '!scm_totn', id: 3236566964, name: 'Tribes of the North' },
-    { pack: 'scm_skaven_clans', id: 3318719948, name: 'Skaven Clans' }, // { pack: 'str_skaven_clans', id: 2986543735, name: 'Skaven Clans' }
+    { pack: 'str_skaven_clans', id: 2986543735, name: 'Skaven Clans' }, // { pack: 'scm_skaven_clans', id: 3318719948, name: 'Skaven Clans' }
     { pack: '!scm_marienburg', id: 3030996759, name: 'Marienburg' },
     { pack: '!!!!!!Champions_of_undeath_merged_fun_tyme', id: 3022054734, name: 'Champions of Undeath' },
     { pack: 'jade_vamp_pol', id: 2880515805, name: 'JBV: Curse of Nongchang' },
     { pack: 'jade_vamp_pol_IotM', id: 3032432016, name: 'JBV: Islanders of the Moon' },
     { pack: 'AAA_dynasty_of_the_damned', id: 2991431203, name: 'Dynasty of the Damned' },
     { pack: '!xou_age_TKExtended', id: 3029928348, name: 'Tomb Kings Extended' },
-    { pack: 'scm_rotblood', id: 3318719201, name: 'Rotblood Tribe' }, // { pack: 'str_rotblood', id: 2838941228, name: 'Rotblood Tribe' }
+    { pack: 'str_rotblood', id: 2838941228, name: 'Rotblood Tribe' }, // { pack: 'scm_rotblood', id: 3318719201, name: 'Rotblood Tribe' }
     { pack: '@xou_emp', id: 2890463744, name: "Sigmar's Heirs" },
     { pack: '@xou_emp_assets', id: 2890463783 },
     { pack: '!scm_empire_secessionists', id: 3030410786, name: 'Empire Secessionists' },
