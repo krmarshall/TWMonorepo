edbd0609de0ace0f52acfecc203d4e8d228b7971
krmarshall <59742056+krmarshall@users.noreply.github.com>
2026-01-10 17:00:36 -0700

    Bump: Vite 7

--- TWPlanner/frontend/vite.config.ts
+++ TWPlanner/frontend/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteCompression from 'vite-plugin-compression';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,8 +9,6 @@ export default defineConfig({
     react(),
     tailwindcss(),
     viteCompression({ algorithm: 'gzip', ext: '.gz', compressionOptions: { level: 9 } }),
-    // Without splitVendor react-responsive doesnt export in builds correctly as of vite 3.1.8 and react-responsive 9.0.0
-    splitVendorChunkPlugin(),
   ],
   build: {
     outDir: '../backend/public',
