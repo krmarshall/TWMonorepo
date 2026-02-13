04e8e4b8b435802f776d7673ba00b87e574b3823
krmarshall <59742056+krmarshall@users.noreply.github.com>
2026-02-12 17:59:25 -0700

    Bump: 7.1.0

--- TWPlanner/frontend/src/data/gameData.ts
+++ TWPlanner/frontend/src/data/gameData.ts
@@ -37,7 +37,7 @@ const gameData: { [key: string]: GameDataInterface } = {
     image: gameImages['vanilla3'],
     factions: factions,
     characters: vanilla3CharactersMemes,
-    updated: 'Jan 15 2026 (7.0.3)',
+    updated: 'Feb 10 2026 (7.1.0)',
     category: 'Base',
   },
   sfo3: {
