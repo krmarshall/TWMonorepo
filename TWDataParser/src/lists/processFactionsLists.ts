908542c45e1fcbc2157c8df13d0462dbcfe17f08
krmarshall <59742056+krmarshall@users.noreply.github.com>
2026-09-03 14:41:24 -0600

    Minor: Add Mixu agent

--- TWDataParser/src/lists/processFactionsLists.ts
+++ TWDataParser/src/lists/processFactionsLists.ts
@@ -323,6 +323,8 @@ const addAgents = [
 
   { agent: 'ksl_rastiltin', subculture: 'wh3_main_sc_ksl_kislev', game: '3' },
 
+  { agent: 'sla_slaaulaan', subculture: 'wh3_main_sc_sla_slaanesh', game: '3' },
+
   // Lege 3
   { agent: 'str_rykarth', subculture: 'wh3_dlc23_sc_chd_chaos_dwarfs', game: '3' },
   { agent: 'str_byrrnoth_grundadrakk', subculture: 'wh_main_sc_dwf_dwarfs', game: '3' },
